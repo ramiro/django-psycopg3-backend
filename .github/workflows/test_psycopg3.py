49e1f990696984df270e4de71977f68780f51d8e
Ramiro Morales <cramm0@gmail.com>
2022-02-12 23:35:28 -0300

    set up DB

---
+++ .github/workflows/test_psycopg3.py
@@ -0,0 +1,32 @@
+# A django settings module to run the Django test suite using Psycopg 3.
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django_psycopg3",
+        "HOST": "localhost",
+        # "NAME": "django",
+        "NAME": "postgres",
+        "USER": "postgres",
+    },
+    "other": {
+        "ENGINE": "django_psycopg3",
+        "HOST": "localhost",
+        "NAME": "django2",
+        "USER": "postgres",
+    },
+}
+
+INSTALLED_APPS = [
+    "django.contrib.postgres"
+]
+
+SECRET_KEY = "django_tests_secret_key"
+
+# Use a fast hasher to speed up tests.
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+USE_TZ = False
